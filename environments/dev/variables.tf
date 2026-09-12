149c5135bbc0af9e208f12b3a13ebe3346df6e0c
ChristianRende22 <codirrl@gmail.com>
2026-09-12 09:30:50 -0600

    Lab 6: backend remoto S3+DynamoDB y workspaces multiambiente

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,8 +1,3 @@
-variable "bucket_name" {
-  description = "Nombre único global del bucket S3 para dev"
-  type        = string
-}
-
 variable "aws_region" {
   description = "Región de AWS"
   type        = string
